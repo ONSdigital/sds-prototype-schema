4aa0acea495bad6c2a9f40dad7fc476fc478c6ca
jamesbryer <james.bryer@ext.ons.gov.uk>
2024-10-29 16:14:02 +0000

    add curly braces

--- send_pubsub_messages.sh
+++ send_pubsub_messages.sh
@@ -1,21 +1,18 @@
+#!/usr/bin/env bash
+
 # Load the environment variables
 source /workspace/new_schema_filepaths.env
 source /workspace/error_directories.env
-NEW_SCHEMA_PUBSUB_TOPIC=$_NEW_SCHEMA_PUBSUB_TOPIC
-SCHEMA_FAILURE_PUBSUB_TOPIC=$_SCHEMA_FAILURE_PUBSUB_TOPIC
+NEW_SCHEMA_PUBSUB_TOPIC=${_NEW_SCHEMA_PUBSUB_TOPIC}  # Changed to use curly braces
+SCHEMA_FAILURE_PUBSUB_TOPIC=${_SCHEMA_FAILURE_PUBSUB_TOPIC}  # Changed to use curly braces
 
-# Print the lists
-echo "NEW_SCHEMA_FILEPATHS: ${NEW_SCHEMA_FILEPATHS[@]}"
-echo "ERROR_DIRECTORIES: ${ERROR_DIRECTORIES[@]}"
 
-# loop through new schemas
-for _NEW_SCHEMA_FILEPATH in "${NEW_SCHEMA_FILEPATHS[@]}"; do
-    # send a pubsub message with the schema file path
-    gcloud pubsub topics publish $NEW_SCHEMA_PUBSUB_TOPIC --message ${_NEW_SCHEMA_FILEPATH}
-done
+# Loop through new schemas
+for NEW_SCHEMA_FILEPATH in "${NEW_SCHEMA_FILEPATHS[@]}"; do
+    # Send a Pub/Sub message with the schema file path
+    gcloud pubsub topics publish ${NEW_SCHEMA_PUBSUB_TOPIC} --message "${NEW_SCHEMA_FILEPATH}"
 
-# loop through error directories
-for _ERROR_DIRECTORY in "${ERROR_DIRECTORIES[@]}"; do
-    # send a pubsub message with the error directory
-    gcloud pubsub topics publish $_SCHEMA_FAILURE_PUBSUB_TOPIC --message ${_ERROR_DIRECTORY}
-done
+# Loop through error directories
+for ERROR_DIRECTORY in "${ERROR_DIRECTORIES[@]}"; do
+    # Send a Pub/Sub message with the error directory
+    gcloud pubsub topics publish ${SCHEMA_FAILURE_PUBSUB_TOPIC} --message "${ERROR_DIRECTORY}"
