e87beed851bdd4d130654280739650202b3de574
jamesbryer <james.bryer@ext.ons.gov.uk>
2024-10-30 11:50:47 +0000

    access repo directly from cloudbuild

--- get_schema_list.sh
+++ get_schema_list.sh
@@ -10,23 +10,35 @@ NEW_SCHEMA_FILEPATHS=()
 ERROR_DIRECTORIES=()
 
 # checkout the repository - BRANCH_NAME is a default substituion in Cloud Build
-git clone https://github.com/ONSdigital/sds-prototype-schema.git /workspace
+# git clone https://github.com/ONSdigital/sds-prototype-schema.git /workspace
 # cd into the repository
-cd /workspace/sds-prototype-schema
+cd /workspace
 
 git checkout $BRANCH_NAME
 
+git fetch --unshallow
+
 # Get the latest commit SHA
 LATEST_COMMIT=$(git rev-parse HEAD)
+if [ $? -ne 0 ]; then
+  echo "Failed to retrieve the latest commit. Exiting."
+  exit 1
+fi
 
-# Get the list of newly added files in the latest commit
-NEW_FILES=$(git diff --name-only --diff-filter=A ${LATEST_COMMIT}~1 ${LATEST_COMMIT})
+# Check if there is a previous commit
+if git rev-parse "${LATEST_COMMIT}~1" >/dev/null 2>&1; then
+  # Get the list of newly added files in the latest commit
+  NEW_FILES=$(git diff --name-only --diff-filter=A "${LATEST_COMMIT}~1" "${LATEST_COMMIT}")
+else
+  # If there is no previous commit, assume all files in the schema_directory are new
+  NEW_FILES=$(find "$SCHEMA_DIRECTORY" -type f)
+fi
 
 # Filter the files to only include new schemas in the schema_directory
 NEW_SCHEMAS=$(echo "${NEW_FILES}" | grep "^$SCHEMA_DIRECTORY/")
 
 # Iterate over each subdirectory in the schema_directory
-for subdir in $(find $SCHEMA_DIRECTORY -mindepth 1 -maxdepth 1 -type d); do
+for subdir in $(find "$SCHEMA_DIRECTORY" -mindepth 1 -maxdepth 1 -type d); do
     # Get the list of new schemas in the subdirectory
     NEW_SCHEMAS_IN_SUBDIR=$(echo "$NEW_SCHEMAS" | grep "^$subdir/")
 
@@ -34,11 +46,11 @@ for subdir in $(find $SCHEMA_DIRECTORY -mindepth 1 -maxdepth 1 -type d); do
     NUM_NEW_SCHEMAS=$(echo "$NEW_SCHEMAS_IN_SUBDIR" | wc -l)
 
     if [ "$NUM_NEW_SCHEMAS" -eq 1 ]; then
-    # If there is exactly one new schema, add it to the NEW_SCHEMA_FILEPATHS list
-    NEW_SCHEMA_FILEPATHS+=("$NEW_SCHEMAS_IN_SUBDIR")
+        # If there is exactly one new schema, add it to the NEW_SCHEMA_FILEPATHS list
+        NEW_SCHEMA_FILEPATHS+=("$NEW_SCHEMAS_IN_SUBDIR")
     elif [ "$NUM_NEW_SCHEMAS" -gt 1 ]; then
-    # If there is more than one new schema, add the subdirectory to the ERROR_DIRECTORIES list
-    ERROR_DIRECTORIES+=("$subdir")
+        # If there is more than one new schema, add the subdirectory to the ERROR_DIRECTORIES list
+        ERROR_DIRECTORIES+=("$subdir")
     fi
 done
 
