f21436d1c0d98b0f8660963c28f68883693c1e63
jamesbryer <james.bryer@ext.ons.gov.uk>
2024-11-04 16:58:40 +0000

    Reset and and check for empty list

--- send_pubsub_messages.sh
+++ send_pubsub_messages.sh
@@ -2,23 +2,27 @@
 
 # Load the environment variables
 source /workspace/new_schema_filepaths.env
-# source /workspace/error_directories.env
+source /workspace/error_directories.env
 NEW_SCHEMA_PUBSUB_TOPIC=${_NEW_SCHEMA_PUBSUB_TOPIC}
 SCHEMA_FAILURE_PUBSUB_TOPIC=${_SCHEMA_FAILURE_PUBSUB_TOPIC}
 
 
 # Loop through new schemas
 for new_schema_filepath in "${NEW_SCHEMA_FILEPATHS[@]}"; do
+    # if error directory is empty, skip
+        if [ -z "$new_schema_filepath" ]; then
+            continue
+        fi
     # Send a Pub/Sub message with the schema file path
     gcloud pubsub topics publish schemas-for-publication --message ${new_schema_filepath}
 done
 
 # Loop through error directories
-# for error_directory in "${ERROR_DIRECTORIES[@]}"; do
-#     # if error directory is empty, skip
-#     if [ -z "$error_directory" ]; then
-#         continue
-#     fi
-#     # Send a Pub/Sub message with the error directory
-#     gcloud pubsub topics publish fail-schema-topic --message ${error_directory}
-# done
+for error_directory in "${ERROR_DIRECTORIES[@]}"; do
+    # if error directory is empty, skip
+    if [ -z "$error_directory" ]; then
+        continue
+    fi
+    # Send a Pub/Sub message with the error directory
+    gcloud pubsub topics publish fail-schema-topic --message ${error_directory}
+done
